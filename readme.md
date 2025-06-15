88dbe502e06290c7d35ad95f62789d39de3fc151
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-06-15 10:19:48 +0530

    use routing with perfection

---
+++ readme.md
@@ -0,0 +1,5 @@
+-- npm init
+-- npm i express
+-- npm i -g nodemon  -> it uses to restart the server automatically
+
+// it has routes order is mandtory.
---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+
+
+
+app.get('/user',(req,res)=>{
+    res.send('user data get suceessusfully');
+})
+
+
+app.post('/user',(req,res)=>{
+    res.send('user data post suceessusfully');
+})
+
+app.use('/user',(req,res)=>{
+    res.send('user data middleware suceessusfully');
+})
+
+
+
+
+app.listen(3000, ()=>{console.log('Server is running on port 3000');});
