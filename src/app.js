96e54293569fd801394e7f2ae1f26d86c42c6ef6
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-06-25 23:47:02 +0530

    code sanitization

--- src/app.js
+++ src/app.js
@@ -26,34 +26,45 @@ app.get("/feed", async (req, res) => {
     // } else {
     //   res.send(userDetails);
     // }
-     res.send(userDetails);
+    res.send(userDetails);
   } catch (error) {
     return res.status(400).send("No users found");
   }
 });
 
-app.delete("/delete", async (req,res)=>{
+app.delete("/delete", async (req, res) => {
   const userid = req.body.userID;
-  try{
-  const deleted = await User.findByIdAndDelete({_id:userid});
-  res.send("User Delete sucessfully!");
-
-  }
-   catch (error) {
+  try {
+    const deleted = await User.findByIdAndDelete({ _id: userid });
+    res.send("User Delete sucessfully!");
+  } catch (error) {
     res.status(400).send("Error creating user: " + error.message);
   }
-})
+});
 
-app.patch("/update", async (req,res)=>{
-  const email = req.body.emailID;
+app.patch("/update/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
   try {
-     await User.findOneAndUpdate({ emailID: email }, data);
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+    const isUpdateAllowed = Object.keys(data).every((k) => {
+      ALLOWED_UPDATES.includes(k);
+    });
+    if (!isUpdateAllowed) {
+      throw new Error("Update not Allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be morethan 10");
+    }
+    await User.findOneAndUpdate({ _id: userId }, data,{
+      returnDocument:"after",
+      runValidators:true
+    });
     res.send("Update Sucessfully!");
   } catch (error) {
     return res.status(400).send("No users found");
   }
-})
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
@@ -17,10 +18,20 @@ const userSchema = new mongoose.Schema(
       lowercase: true,
       unique: true,
       trim: true,
+      validate(values){
+        if(!validator.isEmail(values)){
+            throw new Error("Inavalid email Address: " + values);
+        }
+      }
     },
     password: {
       type: String,
       required: true,
+      validate(values){
+        if(!validator.isStrongPassword(values)){
+            throw new Error("Enter a strong Password: " + values);
+        }
+      }
     },
     age: {
       type: Number,
@@ -34,10 +45,15 @@ const userSchema = new mongoose.Schema(
         }
       },
     },
-    photourl: {
+    photoUrl: {
       default:
         "https://imgs.search.brave.com/66DDLXgQTTuzioBWr-U18A4Kr4IMF-D64A5_hmWNiTE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHJlbWl1bS12ZWN0/b3IvM2Qtc2ltcGxl/LXVzZXItaWNvbi1p/c29sYXRlZF8xNjky/NDEtNjk1NC5qcGc_/c2VtdD1haXNfaXRl/bXNfYm9vc3RlZCZ3/PTc0MA",
       type: String,
+      validate(values){
+        if(!validator.isURL(values)){
+            throw new Error("Inavalid Photo Url: " + values);
+        }
+      }
     },
     about: {
       type: String,
