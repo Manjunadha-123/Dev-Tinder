c777b4d9cddd21ceefcda1e886a6cf7ceb79d499
Guda Manjunadha <gudamanjunadhareddy@gmail.com>
2025-07-19 15:03:03 +0530

    changes in all

--- src/app.js
+++ src/app.js
@@ -8,10 +8,7 @@ const cors = require("cors");
 app.use(
   cors({
     origin: "http://localhost:5173",
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
+    credentials: true,  
   })
 );
 app.use(express.json());
